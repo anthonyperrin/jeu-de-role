1990964b654876e8c5c01e4e6d09ff0f64c6c84c
Anthony Perrin <anthonyperrinff@gmail.com>
2018-11-27 15:03:08 +0100

    Fade transition between maps

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import GridDisplayer from './game/grid';
 
 //Game grid
-const gridDisplayer = new GridDisplayer();
+// const gridDisplayer = new GridDisplayer();
 //Displays the grid
-gridDisplayer.render();
+// gridDisplayer.render();
+
+let code;
+const bckVideo = $('#bckVideo');
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j'];
+$(document).on('keyup', (event) => {
+    if (event.key !== undefined) {
+        code = event.key;
+        bckVideo.fadeOut(1000, () => {
+            bckVideo.attr('src', './assets/video/cave_' + letters[Math.floor(Math.random() * Math.floor(8))] + '_HD_gridless.m4v')
+                .fadeIn(1000);
+        })
+    }
+});
